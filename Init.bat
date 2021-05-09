aeb8da829b9cf78bf7ea791a9fcf47aa5b2bc1a1
rhy3h <ricciyeh880427@gmail.com>
2021-05-09 19:28:22 +0800

    123

--- Init.bat
+++ Init.bat
@@ -3,5 +3,5 @@ python manage.py makemigrations
 python manage.py migrate
 python manage.py createsuperuser
 
-python manage.py makemigrations djangoapp
+python manage.py makemigrations rhy3h_stocksite
 python manage.py migrate
